abd48bf4567b6a991466bec24a9ac53f24fafc68
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2018-08-19 11:57:17 -0400

    introducing default export

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-// import './utils.js';
-// import { square, add } from './utils.js';
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(4,6));
+import './utils.js';
+import subtract, { square, add } from './utils.js';
+console.log('app.js is running');
+console.log(square(4));
+console.log(add(4,6));
+console.log(subtract(100, 81));
 
-import { isAdult, canDrink } from './person.js';
+// import { isAdult, canDrink } from './person.js';
 
-console.log(isAdult(19));
-console.log(canDrink(19));
+// console.log(isAdult(19));
+// console.log(canDrink(19));
--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,12 @@
-// console.log('utils.js is runnning');
+  console.log('utils.js is runnning');
 
-// const square = (x) => x * x;
-// const add = (a, b) => a + b;
+  const square = (x) => x * x;
+  const add = (a, b) => a + b;
+  const subtract = (a, b) => a - b;
 
-// export { square, add };
+  export { square, add, subtract as default };
 
-// // we can use
-// // export const square = (x) => x * x;
+// we can use
+// export const square = (x) => x * x;
 
-// //export - default export - named exports
+//export - default export - named exports

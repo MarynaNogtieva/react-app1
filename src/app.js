2c07829f34366074db2523775755368b0a3bae62
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2018-08-19 11:32:10 -0400

    start using export import statements

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
+// import './utils.js';
+import { square } from './utils.js';
 console.log('app.js is running');
+console.log(square(4));
---
+++ src/utils.js
@@ -0,0 +1,7 @@
+console.log('utils.js is runnning');
+
+const square = (x) => x * x;
+
+export { square };
+
+//export - default export - named exports
